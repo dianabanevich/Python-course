c3cf55755f300b07adbf51701c28dada4318e486
dianabanevich <banevicdiana@gmail.com>
2022-01-22 18:32:13 +0300

    add lesson_1 file

---
+++ lesson_1.py
@@ -0,0 +1,55 @@
+a = 'My name is Diana'
+print(a)
+
+b = 22
+print(b)
+
+c = 53.2
+print(c)
+
+d = bytes ([104, 101, 108, 108, 111])
+print (d)
+
+e = list('hello')
+print(e)
+
+f = tuple('world')
+print(f)
+
+g = set('hello world')
+print(g)
+
+h = frozenset('hello world!')
+print(h)
+
+i = dict[a, b, c]
+print(i)
+
+print('str:',a ,',', 'int:', b ,',', 'float:', c ,',', 'bytes:', d,',',
+      'list:', e ,',', 'turple:', f ,',', 'set:', g ,',', 'frozenset:', h ,',', 'dict:', i )
+
+name = 'Diana'
+surname = 'Banevich'
+all_name = name + ' ' + surname
+print(all_name)
+
+a = 15
+b = 10
+c = a + b
+print(c)
+
+d = a / b
+print(d)
+
+e = a * b
+print(e)
+
+f = a // b
+print(f)
+
+g = a % b
+print(g)
+
+h = (7 + 12) ** 3 + 7 * 4 - 44 / 2 ** 4
+print(h)
+
